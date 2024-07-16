b629fa816288d9455617b1ddf7c88f29c85c3f6a
b_g <b_g@dsm.hs.kr>
2024-07-16 16:12:28 +0900

    setting

---
+++ lib/view/page_manager/page_manager_screen.dart
@@ -0,0 +1,54 @@
+import 'package:application_frontend/view/chart/chart_screen.dart';
+import 'package:application_frontend/view/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class PageManagerScreen extends StatefulWidget {
+  const PageManagerScreen({super.key});
+
+  @override
+  State<PageManagerScreen> createState() => _PageManagerScreenState();
+}
+
+class _PageManagerScreenState extends State<PageManagerScreen> {
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> pages = [
+      HomeScreen(),
+      ChartScreen(),
+    ];
+
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: pages,
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          IconButton(
+              onPressed: () => _pageController.jumpToPage(0),
+              icon: Icon(Icons.home)),
+          IconButton(
+              onPressed: () {
+                _pageController.jumpToPage(1);
+              },
+              icon: Icon(Icons.home)),
+        ],
+      ),
+    );
+  }
+}
